9936b8910054d6bca208e088b49e5c6c4844499e
Renaud CARON <renaud.caron1@gmail.com>
2022-11-18 23:20:02 +0100

    hit/reflect fixes

--- ray3f.cpp
+++ ray3f.cpp
@@ -2,7 +2,11 @@
 
 Ray3f::Ray3f(Vector3f origin_, Vector3f direction_) {
   origin = origin_;
-  direction = direction_;
+  float direction_norm = norm(direction_);
+  if (direction_norm<1e-3) {
+    throw "Direction can't be 0";
+  }
+  direction = direction_/direction_norm;
 }
 
 std::ostream & operator<< (std::ostream &st, const Ray3f &r) {
--- sphere.cpp
+++ sphere.cpp
@@ -13,10 +13,10 @@ Sphere::Sphere(Vector3f origin_, float radius_, Material matter_) {
 // https://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
 bool Sphere::isHit(Ray3f ray) const {
   Vector3f o = ray.origin;
-  Vector3f u_hat = ray.direction/norm(ray.direction);
+  Vector3f u = ray.direction;
   Vector3f c = origin;
   float r = radius;
-  float delta = pow(dot_product(u_hat,o-c),2) - (pow(norm(o-c),2)+pow(r,2));
+  float delta = pow(dot_product(u,o-c),2) - (pow(norm(o-c),2)-pow(r,2));
   return (delta>0); // voir pour mettre un 1e-3 ou quoi
 }
 
@@ -24,21 +24,21 @@ bool Sphere::isHit(Ray3f ray) const {
 // http://www.3dkingdoms.com/weekly/weekly.php?a=2
 Ray3f Sphere::reflect(Ray3f ray) const {
   Vector3f o = ray.origin;
-  Vector3f u_hat = ray.direction/norm(ray.direction);
+  Vector3f u = ray.direction;
   Vector3f c = origin;
   float r = radius;
-  float delta = pow(dot_product(u_hat,o-c),2) - (pow(norm(o-c),2)+pow(r,2));
-  float d1 = -dot_product(u_hat,o-c)+sqrt(delta);
-  float d2 = -dot_product(u_hat,o-c)-sqrt(delta);
+  float delta = pow(dot_product(u,o-c),2) - (pow(norm(o-c),2)-pow(r,2));
+  float d1 = -dot_product(u,o-c)+sqrt(delta);
+  float d2 = -dot_product(u,o-c)-sqrt(delta);
   Vector3f intersection;
   if (abs(d1)<abs(d2)) {
-    intersection = o + d1*u_hat;
+    intersection = o + d1*u;
   }
   else {
-    intersection = o + d2*u_hat;
+    intersection = o + d2*u;
   }
   Vector3f N = c - intersection;
-  return Ray3f(intersection, -2*dot_product(u_hat, N)*N+u_hat);
+  return Ray3f(intersection, -2*dot_product(u, N)*N+u);
 }
 
 std::ostream & operator<< (std::ostream &st, const Sphere &s) {

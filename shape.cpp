4af8f625abb6e713ccfd09651bd4d6e6d6af7b62
Renaud CARON <renaud.caron1@gmail.com>
2022-11-21 19:38:55 +0100

    mini fix2.0

--- shape.cpp
+++ shape.cpp
@@ -4,7 +4,7 @@ bool isEnlightened(Shape** shapes, int nb_shapes, Shape* last_shape, Vector3f li
   float light_dist = norm(light - point);
   for (int i = 0; i < nb_shapes; i++) {
     if (last_shape != shapes[i]) {
-      Ray3f light_path(light, light-point);
+      Ray3f light_path(light, point-light);
       if (shapes[i]->isHit(light_path)) {
         Vector3f potential_obstacle = shapes[i]->reflect(light_path).origin;
         if (light_dist > norm(light - potential_obstacle)) {
